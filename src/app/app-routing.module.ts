157ebf1abe78090854efe4aefd92880d509719cb
Edmar Prado <edmarssouzap@gmail.com>
2021-10-25 03:30:55 -0300

    Modificado o form na navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { JogadoresComponent } from './jogadores/jogadores.component';
 import { HomeComponent } from './home/home.component';
 
+// Rota para home e jogadores criadas e apontando para o componente
 const routes: Routes = [
   {path: 'jogadores', component: JogadoresComponent},
   {path: 'home', component: HomeComponent}

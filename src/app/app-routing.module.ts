34a4284ff4210cd0ea204f97552835119c2966a5
Edmar Prado <edmarssouzap@gmail.com>
2021-10-25 03:51:43 -0300

    Alteracoes no nav e rota raiz

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,9 @@ import { HomeComponent } from './home/home.component';
 // Rota para home e jogadores criadas e apontando para o componente
 const routes: Routes = [
   {path: 'jogadores', component: JogadoresComponent},
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  // Rota criada para quando a página iniciada é o raiz (/)
+  {path: '', component: HomeComponent}
 ];
 
 @NgModule({
